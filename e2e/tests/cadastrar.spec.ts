19ad6e0c2998ed1b85575f81a9c0d4af55d4f9f7
Paulo Andrade <pcdajr@gmail.com>
2026-07-17 22:46:15 -0300

    teste

--- e2e/tests/cadastrar.spec.ts
+++ e2e/tests/cadastrar.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
+test.describe("Página inicial", () => {
 test('Visitar página inicial', async ({ page }) => {
   
-
-  await page.goto('/');
+  await page.goto("/");
   
-
+  });
 });
 
