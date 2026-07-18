c9d04d4248f58efe3bd92b3552a40d81a100d3a2
Paulo Andrade <pcdajr@gmail.com>
2026-07-17 23:23:59 -0300

    capturando elementos para a camada de components

--- e2e/components/elementoCorpo.ts
+++ e2e/components/elementoCorpo.ts
@@ -7,3 +7,6 @@
 // getByRole('link', { name: 'Phones' });
 // getByRole('link', { name: 'Laptops' });
 // getByRole('link', { name: 'Monitors' });
+
+// adicionar ao carrinho (dentro da página do produto)
+// getByRole('link', { name: 'Add to cart' });
