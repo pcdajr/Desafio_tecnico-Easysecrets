05117146253ca15476a93a958536052fabd7c0ca
Paulo Andrade <pcdajr@gmail.com>
2026-07-17 22:18:52 -0300

    url base config

--- e2e/tests/cadastrar.spec.ts
+++ e2e/tests/cadastrar.spec.ts
@@ -3,8 +3,7 @@ import { test, expect } from '@playwright/test';
 test('Visitar página inicial', async ({ page }) => {
   
 
-  await page.goto('https://www.demoblaze.com/#');
-  
+  await page.goto('/');
   
 
 });
